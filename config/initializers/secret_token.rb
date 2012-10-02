53a56fca74ab5fffc35ec1147dc2a92d181dcc44
Preeti Gupta <guptapreeti.india@gmail.com>
2012-10-02 19:36:12 +0530

    Set up a Ruby on Rails Hello World! application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CashFerret::Application.config.secret_token = '418cec0dbcbd07875a2dc78622f20f313a799f126081f66e0ab2a516dfe122de9d2d43bf731057dc30e4d1d05f73319d60125399cc08197478b4004fc9bfbcbd'
